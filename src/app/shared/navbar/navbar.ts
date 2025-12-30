f78802c6fe7d712f6258d5e06af343bb27de3408
giorgio.xo <gikhokhonishvili@gmail.com>
2025-12-30 04:08:57 +0400

    feat: add fonts and dashboard

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'creme-navbar',
   imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
